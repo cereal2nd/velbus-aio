140c4e56191ea65ace262a8893a425410ca98e93
Maikel Punie <maikel.punie@gmail.com>
2025-10-20 17:37:03 +0200

    more work on specs until 0x1E

--- examples/load_modules.py
+++ examples/load_modules.py
@@ -9,10 +9,11 @@ from velbusaio.controller import Velbus
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--connect", help="Connection string", default="192.168.1.254:8445")
+parser.add_argument("--vlp", help="VLP file path", default=None)
 args = parser.parse_args()
 
 
-async def main(connect_str: str):
+async def main(connect_str: str, vlp: str):
     # SET THE connection params below
     # example via signum:
     #   velbus = Velbus("tls://192.168.1.9:27015")
@@ -20,7 +21,7 @@ async def main(connect_str: str):
     #   velbus = Velbus("192.168.1.9:27015")
     # example via serial device
     #   velbus = Velbus("/dev/ttyAMA0")
-    velbus = Velbus(connect_str)
+    velbus = Velbus(dsn=connect_str, vlp_file=vlp)
     await velbus.connect()
     await velbus.start()
     for mod in (velbus.get_modules()).values():
@@ -31,4 +32,4 @@ async def main(connect_str: str):
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger("asyncio").setLevel(logging.DEBUG)
-asyncio.run(main(args.connect), debug=False)
+asyncio.run(main(args.connect, args.vlp), debug=False)
