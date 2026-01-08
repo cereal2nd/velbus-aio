062309232c6dc76dc721aabdfcd620d26be48225
Maikel Punie <maikel.punie@gmail.com>
2026-01-08 09:00:48 +0100

    Implement memory block reading, this limits the number of messages on the bus

--- tests/module_name_test.py
+++ tests/module_name_test.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from velbusaio.messages.memory_data import MemoryDataMessage
+from velbusaio.messages.memory_data_block import MemoryDataBlockMessage
 from velbusaio.module import Module
 
 
@@ -42,22 +42,26 @@ async def test_module_name(name):
     module_address = 1
     module_type = 0x0E  # VMB1TC
 
-    memory = {}
-    for i in range(16):
-        memory[0xF0 + i] = 0xFF
+    # Prepare memory with 16 bytes initialized to 0xFF
+    memory_bytes = [0xFF] * 16
     for i, c in enumerate(name):
-        memory[0xF0 + i] = ord(c)
+        memory_bytes[i] = ord(c)
+
+    # Build memory in 4-byte blocks
+    memory = {}
+    for i in range(0, 16, 4):
+        memory[0xF0 + i] = memory_bytes[i:i + 4]
 
     m = Module(module_address, module_type)
+    m._use_cache = False
     await m.initialize(MockWriter(), MockController())
     m._log = logging.getLogger("velbus-module")
 
     for addr, data in memory.items():
-        msg = MemoryDataMessage()
+        msg = MemoryDataBlockMessage()
         msg.high_address = addr >> 8
         msg.low_address = addr & 0xFF
         msg.data = data
-        await m._process_memory_data_message(msg)
-        print(m)
+        await m._process_memory_data_block_message(msg)
 
     assert m.get_name() == name
