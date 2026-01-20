cd1279556d50d773a01ed4138f2ac2757ace7353
Maikel Punie <maikel.punie@gmail.com>
2026-01-20 18:49:52 +0100

    fix vlp file example

--- examples/vlp_file.py
+++ examples/vlp_file.py
@@ -6,7 +6,7 @@ import logging
 import pprint
 import sys
 
-from velbusaio.vlp_reader import vlpFile
+from velbusaio.vlp_reader import VlpFile
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--path", help="Path of the vlp file", required=True)
@@ -14,7 +14,7 @@ args = parser.parse_args()
 
 
 async def main(path: str):
-    vlp = vlpFile(path)
+    vlp = VlpFile(path)
     await vlp.read()
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(vlp.get())
